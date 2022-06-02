94dc983dbd0b447b00cd1f526f2fc3f65173378c
cat-daniels <84743435+cat-daniels@users.noreply.github.com>
2022-06-03 01:22:19 +1200

    added log-in and auth system

---
+++ app.py
@@ -0,0 +1,108 @@
+from flask import Flask, render_template, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from wtforms import StringField,PasswordField,SubmitField
+from wtforms.validators import InputRequired,Length,ValidationError
+from flask_wtf import FlaskForm
+from datetime import datetime
+from flask_bcrypt import Bcrypt
+app = Flask(__name__)
+
+#Config Database
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY']='secretkey'
+
+#So we can hash passwords
+bcrypt=Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
+
+
+#Model for Users
+class Users(UserMixin, db.Model):
+    __tablename__ = 'Users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    #unique = true means unique username only
+    password = db.Column(db.String(256), nullable=False)
+    is_Admin = db.Column(db.Boolean, nullable=False, default=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+class RegisterForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Register')
+
+    def validate_username(self, username):
+        existing_user_username = Users.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    username = StringField(validators=[
+                           InputRequired(), Length(min=4, max=20)], render_kw={"placeholder": "Username"})
+
+    password = PasswordField(validators=[
+                             InputRequired(), Length(min=8, max=20)], render_kw={"placeholder": "Password"})
+
+    submit = SubmitField('Login')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/profile', methods = ['GET','POST'])
+@login_required
+def profile():
+    return render_template('profile.html')
+
+@app.route('/login', methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('profile'))
+    return render_template('login.html',form=form)
+
+@app.route('/register',methods = ['GET','POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_pwd = bcrypt.generate_password_hash(form.password.data)
+        new_user = Users(username = form.username.data,password = hashed_pwd)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
+    return render_template('register.html',form=form)
+
+@app.route('/logout', methods = ['GET','POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+if __name__ == '__main__' :
+    app.run(debug=True)
+
+
