0c5cd7817a347834303f08f22bcdf67fd0a305d2
cat-daniels <84743435+cat-daniels@users.noreply.github.com>
2022-06-03 03:37:19 +1200

    Revert "push to undo work"
    This reverts commit d63f08c3d21eb1acf3063a4cd6a3621706f258e5.

--- models.py
+++ models.py
@@ -12,19 +12,3 @@ class Users(UserMixin, db.Model):
     password = db.Column(db.String(256), nullable=False)
     is_Admin = db.Column(db.Boolean, nullable=False, default=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
-
-    class Conversions(UserMixin, db.Model):
-        __tablename__ = 'Conversions'
-        id = db.Column(db.Integer, primary_key=True)
-        user_id = db.Column(db.Integer)
-        username = db.Column(db.String(50), nullable=False)
-        fromCurrency = db.Column(db.String(3), nullable=False)
-        toCurrency = db.Column(db.String(3), nullable=False)
-        fromValue = db.Column(db.String(20), nullable=False)
-        toValue = db.Column(db.String(20), nullable=False)
-        rate = db.Column(db.String(15), nullable=False)
-        date_saved = db.Column(db.DateTime, default=datetime.utcnow)
-    
-    def __repr__(self):
-        template = '{0.user_id} {0.username} {0.fromCurrency} {0.toCurrency} {0.fromValue} {0.toValue} {0.rate} {0.date_saved}'
-        return template.format(self)
